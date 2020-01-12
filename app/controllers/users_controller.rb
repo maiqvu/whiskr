483c7217578af141b496ad3dad58168674c23b31
mai <user@Users-MacBook-Pro.local>
2020-01-12 18:38:50 +1100

    basic model-view-controller for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,53 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
+
 
   def create
+    @user = User.create( user_params )
+
+    if @user.persisted?   # Log them in automatically after successful sign-up.
+      session[:user_id] = @user.id
+      redirect_to user_path( @user.id )
+    else   # If error during sign-up, show the form again.
+      render :new
+    end
   end
+
 
   def index
+    @users = User.all
   end
+
 
   def show
+    @user = User.find( params[:id] )
   end
+
 
   def edit
+    @user = User.find( params[:id] )
   end
+
 
   def update
+    user = User.find( params[:id] )
+    user.update( user_params )
+    redirect_to user_path( user.id )
   end
 
+
   def destroy
+    User.destroy( params[:id] )
+    redirect_to users_path
   end
+
+  # strong params
+  private
+  def user_params
+    params.require( :user ).permit( :name, :email, :bio )
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+u1 = User.create! name: "mia", email: "whiskr@whiskr.com", bio: "whiskr overlord"
+u2 = User.create! name: "Pawdry Hepburn", email: "hepburn@mail.com", bio: "If you're into bad girls, swipe left because I'm a good girl."
+u3 = User.create! name: "Leopawrdo DiCatrio", email: "sinker@titanic.com", bio: "Total stud! Swipe right or you'll regret for life."
+u4 = User.create! name: 'Jude Paw', email: "drwatson@bakerst.com", bio: "Enjoy long walks to the kitchen to find cheese."
+u5 = User.create! name: 'Oprah Whisker', bio: "World famous. Mogul. Alpha female."
+
+print "Done creating #{User.count} users: "
+puts "#{User.pluck(:name).join(', ')}."
