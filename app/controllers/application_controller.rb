3c798d8b75a7e2620fc1c1841c6da8479f353a3e
mai <user@Users-MacBook-Pro.local>
2020-01-14 16:51:39 +1100

    fixed nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
     
     before_action :fetch_user
 
-    # private
-
     def fetch_user
         if session[:user_id].present?
             @current_user = User.find_by( id: session[:user_id] )
@@ -23,6 +21,11 @@ class ApplicationController < ActionController::Base
             flash[:error] = 'You do not have authorization to view this page.'
             redirect_to dashboard_path
         end
+    end
+
+    # To render the '_nav' partial form on all pages except the landing page.
+    def nav_check
+        @skip_nav = true
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :check_if_logged_in, except: [:new, :create]
   before_action :check_if_admin, only: [:index]
+  before_action :nav_check
 
   def new
     @user = User.new
